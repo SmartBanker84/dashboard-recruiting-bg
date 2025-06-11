75ee76d184d864a18fe036d0172eb36ecc433aca
SmartBanker84 <simone.capo@yahoo.it>
2025-06-12 00:05:02 +0200

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,8 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabase = createClient(
+export const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.SUPABASE_SERVICE_ROLE_KEY! // ⚠️ non usare anon key qui
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 )
-
-export { supabase }
