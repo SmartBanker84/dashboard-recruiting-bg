90c62a82bbab5ea17b63280909e677987d7859d8
SmartBanker84 <simone.capo@yahoo.it>
2025-06-12 00:02:19 +0200

    Create server.ts

---
+++ lib/supabase/server.ts
@@ -0,0 +1,9 @@
+// lib/supabase/server.ts
+import { createClient } from '@supabase/supabase-js'
+
+const supabase = createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY! // ⚠️ non usare anon key qui
+)
+
+export { supabase }
