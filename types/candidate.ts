53999a57ead9d51993f343d4b418955c0124b2c9
SmartBanker84 <simone.capo@yahoo.it>
2025-06-12 00:11:15 +0200

    Create candidate.ts

---
+++ types/candidate.ts
@@ -0,0 +1,12 @@
+export interface Candidate {
+  id: string
+  name: string
+  email: string
+  birthdate?: string
+  company?: string
+  gender?: string
+  segment?: string
+  status?: string
+  note?: string
+  created_at?: string
+}
