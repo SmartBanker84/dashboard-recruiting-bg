c0c01ef85698b9faa828b9455c8e8627e65ca30c
SmartBanker84 <simone.capo@yahoo.it>
2025-06-16 22:51:32 +0200

    Update index.ts

--- lib/supabase/index.ts
+++ lib/supabase/index.ts
@@ -1,4 +1,2 @@
-// lib/supabase/index.ts
-
-export { supabase } from './client'            // Supabase lato client (browser)
+export { supabase } from './client' // Supabase lato client (browser)
 export { supabase as supabaseServer } from './server' // Supabase lato server (API, SSR)
