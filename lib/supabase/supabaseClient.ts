31ba1b8ccf4f4d130f0c362235f51288baff7ed2
SmartBanker84 <simone.capo@yahoo.it>
2025-06-18 22:30:43 +0200

    Delete lib/supabase/supabaseClient.ts

--- lib/supabase/supabaseClient.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-import { Database } from "@/types/supabase"; // usa 'any' se non hai il tipo Database
-
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
-// Client per l'uso lato client (browser)
-export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey);
